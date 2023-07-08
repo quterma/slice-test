4d0a55a8d562a4b2822a9e365eb63d21d973dd81
Quterma <wolf921@mail.ru>
2023-07-08 23:40:20 +0300

    first commit

---
+++ src/api/data-seeder.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+const createTable = numRows => {
+	const statuses = ["Pending Signatures", "Approved By Corp.", "Offered"];
+
+	return new Array(numRows)
+		.fill({
+			geo: "Denmark",
+			certificates: "GR-1",
+			source: "Manual",
+			plan: "Acne Israel",
+			date: "01/01/22",
+			prefix: "OC",
+		})
+		.map(row => ({ ...row, status: statuses[Math.floor(Math.random() * statuses.length)] }));
+};
+
+fs.writeFile("src/api/table.json", JSON.stringify(createTable(200)), "utf8", err => {
+	if (err) throw err;
+	console.log("complete");
+});
